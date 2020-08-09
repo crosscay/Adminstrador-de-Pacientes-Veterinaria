c472c65cadd9041ce53274d95b6a242a80b5141c
crosscay <caicedocarlos0306@gmail.com>
2020-08-09 02:14:27 -0500

    Updates

---
+++ precache-manifest.1ecfe01c581d6943296e4bf36c03b1b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b93212c541c8a2f24378de2be0737bf",
+    "url": "/Administrador_de_Pacientes_Veterinaria_React/index.html"
+  },
+  {
+    "revision": "079f6308d56651c4157b",
+    "url": "/Administrador_de_Pacientes_Veterinaria_React/static/css/main.1de405d2.chunk.css"
+  },
+  {
+    "revision": "d2fc68571d1ab043a9d3",
+    "url": "/Administrador_de_Pacientes_Veterinaria_React/static/js/2.2b529cbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Administrador_de_Pacientes_Veterinaria_React/static/js/2.2b529cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079f6308d56651c4157b",
+    "url": "/Administrador_de_Pacientes_Veterinaria_React/static/js/main.df918950.chunk.js"
+  },
+  {
+    "revision": "aaf33286d1c55805db31",
+    "url": "/Administrador_de_Pacientes_Veterinaria_React/static/js/runtime-main.bbe337a7.js"
+  }
+]);
